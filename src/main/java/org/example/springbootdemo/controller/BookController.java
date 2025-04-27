7f7ad3ed59bb048cd9205d6745a5b18757941313
yang pei <yangpei@kingmed.com.cn>
2025-04-27 11:19:33 +0800

    feat: 项目初始化

---
+++ src/main/java/org/example/springbootdemo/controller/BookController.java
@@ -0,0 +1,16 @@
+package org.example.springbootdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Value("sangongchi")
+    String bookName;
+
+
+
+}
---
+++ src/main/java/org/example/springbootdemo/repository/BookRepository.java
@@ -0,0 +1,4 @@
+package org.example.springbootdemo.repository;
+
+public interface BookRepository {
+}
